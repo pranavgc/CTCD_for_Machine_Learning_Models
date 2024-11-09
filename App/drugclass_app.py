2bbbb8b1322d8e5a2d64e329f9db9bfc478ff50e
pranavgc <92306584+pranavgc@users.noreply.github.com>
2024-11-09 21:48:36 +0530

    Update drugclass_app.py

--- App/drugclass_app.py
+++ App/drugclass_app.py
@@ -2,21 +2,17 @@ import gradio as gr
 import skops.io as sio
 
 unknown_types = sio.get_untrusted_types(file="Model/drug_pipeline.skops")
-print(unknown_types)
+#print(unknown_types)
 model=sio.load("Model/drug_pipeline.skops", trusted=unknown_types)
 
 def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
-    Predict drugs based on patient features.
-
+    """Predict drugs based on patient features.
     Args:
         age (int): Age of patient
         sex (str): Sex of patient 
         blood_pressure (str): Blood pressure level
         cholesterol (str): Cholesterol level
-        na_to_k_ratio (float): Ratio of sodium to potassium in blood
-
-    Returns:
-        str: Predicted drug label
+        na_to_k_ratio (float): Ratio of sodium to potassium in blood"""
     
     features = [age, sex, blood_pressure, cholesterol, na_to_k_ratio]
     predicted_drug = model.predict([features])[0]
@@ -34,9 +30,10 @@ inputs = [
 outputs = [gr.Label(num_top_classes=5)]
 
 examples = [
-    [30, "M", "HIGH", "NORMAL", 15.4],
-    [35, "F", "LOW", "NORMAL", 8],
-    [50, "M", "HIGH", "HIGH", 34],
+    [25, "M","LOW", "NORMAL" , 25.4],
+    [35, "F", "HIGH", "NORMAL", 9],
+    [55, "M", "HIGH", "HIGH", 24],
+
 ]
 
 title = "Drug Classification Model"
@@ -51,8 +48,8 @@ gr.Interface(
     title=title,
     description=description,
     article=article,
-    theme=gr.themes.Soft(),
-).launch(share=True)
+    theme=gr.themes.Citrus(),
+).launch()
 
 
 
